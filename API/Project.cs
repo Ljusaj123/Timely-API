bd6deef99f6e5a401f8ae6897e6aa8b649087e92
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 01:33:36 +0200

    created namespace of projects

---
+++ API/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace API
+{
+    public class Project
+    {
+
+        public DateTime Date { get; set; }
+
+        public string StartDateString { get; set; }
+
+        public string StopDateString { get; set; }
+
+        public string? Summary { get; set; }
+    }
+}
