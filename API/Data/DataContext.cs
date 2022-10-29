ca3b52941dcf700bdfbbe3be761a3b76967d6cc6
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 12:24:43 +0200

    created data context

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataaContext :DbContext
+    {
+        public DataaContext(DbContextOptions<DataaContext> options) : base(options) { }
+
+        public DbSet<Project> Projects => Set<Project>();
+    }
+}
