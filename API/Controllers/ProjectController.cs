83c6fb8771c99ffd80737df85449bd91c31143cd
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 12:45:47 +0200

    created CRUD operation

--- API/Controllers/ProjectController.cs
+++ API/Controllers/ProjectController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -7,24 +9,67 @@ namespace API.Controllers
     [ApiController]
     public class ProjectController : ControllerBase
     {
+        private readonly DataaContext _context;
+        public ProjectController(DataaContext context)
+        {
+            _context= context;
+        
+        }
         [HttpGet]
 
         public async Task<ActionResult<List<Project>>> GetProjects() 
         {
-            return new List<Project>
+            return Ok(await _context.Projects.ToListAsync());        
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<Project>>> CreateProject(Project project)
+        {
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Projects.ToListAsync());
+
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult<List<Project>>> UpdateProject(Project project)
+        {
+            var dbProject = await _context.Projects.FindAsync(project.Id);
+            if (dbProject == null)
+            {6
+                return BadRequest("hero not found");
+
+            }
+            dbProject.ProjectName = project.ProjectName;
+            dbProject.StartDateString = project.StartDateString;
+            dbProject.StopDateString = project.StopDateString;
+            dbProject.Duration = project.Duration;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Projects.ToListAsync());
+
+
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<List<Project>>> DeleteProject(int id)
+        {
+            var dbProject = await _context.Projects.FindAsync(id);
+            if (dbProject == null)
             {
-                new Project
-                {
-                    ProjectName="project1",
-                    StartDateString="10.10.2022 10:45",
-                    StopDateString="10.10.2022 10:55",
-                    Duration="00:10"
+                return BadRequest("hero not found");
 
+            }
+            _context.Projects.Remove(dbProject);
+            await _context.SaveChangesAsync();
 
-                }
+            return Ok(await _context.Projects.ToListAsync());
 
-            };
-        
         }
     }
 }
