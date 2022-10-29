08d087259c5de36a42a0a68b3e8087711094bc26
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 11:52:32 +0200

    created controller for projects, one example set for get method

---
+++ API/Controllers/ProjectController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<Project>>> GetProjects() 
+        {
+            return new List<Project>
+            {
+                new Project
+                {
+                    ProjectName="project1",
+                    StartDateString="10.10.2022 10:45",
+                    StopDateString="10.10.2022 10:55",
+                    Duration="00:10"
+
+
+                }
+
+            };
+        
+        }
+    }
+}
